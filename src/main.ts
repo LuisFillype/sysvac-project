de0ec8644828c9c4de9951e425b0ccce92bbec84
LuisFillype <l.fillype@gmail.com>
2022-05-10 23:50:15 -0300

    feat: configure swagger and fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { contentParser } from 'fastify-multer/lib';
+import { fastifyHelmet } from 'fastify-helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+  app.register(contentParser);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sysvac')
+    .setDescription('This is a API documentation for Sysvac project.')
+    .setVersion('1.0')
+    .addTag('Sysvac v1')
+    .addBearerAuth({
+      type: 'http',
+      description:
+        'Example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, () => {
+    console.log('Server is ready at: http://localhost:3000');
+  });
 }
 bootstrap();
