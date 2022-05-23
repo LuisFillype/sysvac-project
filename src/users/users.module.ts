e219fe35f083104aa93fda2ad5711aa22c5d5b75
LuisFillype Vilela Cunha <l.fillype@gmail.com>
2022-05-23 14:28:11 -0300

    feat: add hash provider on module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
+import BCryptHashProvider from 'src/providers/hashProdiver';
 import { usersProvider } from './providers/user.providers';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -7,6 +8,6 @@ import { UsersService } from './users.service';
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [...usersProvider, UsersService],
+  providers: [...usersProvider, UsersService, BCryptHashProvider],
 })
 export class UsersModule {}
