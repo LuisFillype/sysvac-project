c742235cb2bed55702b0bb4f0b04d34e0c37fbba
LuisFillype Vilela Cunha <l.fillype@gmail.com>
2022-06-13 13:26:04 -0300

    style: remove useless import

--- src/vacinas/entities/vacina.entity.ts
+++ src/vacinas/entities/vacina.entity.ts
@@ -1,10 +1,4 @@
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  ManyToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'vacinas' })
 export class Vacina extends BaseEntity {
